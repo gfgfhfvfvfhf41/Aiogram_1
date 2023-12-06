f962e0e431b075ef186ebf3caf06037d83adfb05
gfgfhfvfvfhf41 <54802997+gfgfhfvfvfhf41@users.noreply.github.com>
2023-12-06 05:31:08 +0300

    Update Program.cs

--- CMPHandler/Handler/Program.cs
+++ CMPHandler/Handler/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Text.RegularExpressions;
 
-STLib.Main.Init(@"C:\Users\artem\source\repos\MVP\CMPHandler\model.json");
+STLib.Main.Init(@"C:\Users\gfgfhfvfvfhf41\source\repos\MVP\CMPHandler\model.json");
 Console.WriteLine(Regex.Replace(Console.ReadLine().ToLower(), @"[^\w\s]", ""));
 Console.WriteLine(STLib.Main.GetAnswer(Console.ReadLine()));
